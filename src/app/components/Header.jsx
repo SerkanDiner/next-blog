2fafb88b5a52590be2e897b3e0f29ee31f96440f
SerkanDiner <serkandiner1741@gmail.com>
2025-03-04 14:08:59 +0000

    Yesterday, I tried to add a footer for the Experience Book, but today I commented it out to disable it until it's needed next time.

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -37,8 +37,8 @@ export default function Header() {
         href='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          Looking For Sunset
+        <span className='px-2 py-1 bg-gradient-to-r from-blue-300 via-blue-400 to-blue-600 rounded-lg text-'>
+          Experince Book
         </span>
         
       </Link>
@@ -56,7 +56,7 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button
+      <Button
           className='w-12 h-10 hidden sm:inline'
           color='gray'
           pill
