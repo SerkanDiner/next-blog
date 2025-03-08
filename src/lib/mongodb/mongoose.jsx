724e93fc121602e6ec4c84ae31ad5eb9d9a81962
SerkanDiner <serkandiner1741@gmail.com>
2025-03-08 10:18:48 +0000

    "Removed deprecated MongoDB options"

--- src/lib/mongodb/mongoose.jsx
+++ src/lib/mongodb/mongoose.jsx
@@ -12,8 +12,7 @@ if (initialized) {
 try{
 await mongoose.connect(process.env.MONGODB_URI, {
     dbName: 'next-blog',
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    
 } );
 
 console.log('Connected to MongoDB');
