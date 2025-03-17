6846f0f5d1aaea1e2e26eb6507b5641f8c4eb045
SerkanDiner <serkandiner1741@gmail.com>
2025-03-17 15:33:25 +0000

    did some changes on mongodb

--- src/lib/mongodb/mongoose.jsx
+++ src/lib/mongodb/mongoose.jsx
@@ -3,41 +3,20 @@ import mongoose from 'mongoose';
 let initialized = false;
 
 export const connect = async () => {
-    mongoose.set('strictQuery', true);
-
-    if (initialized) {
-        console.log('Already connected to MongoDB');
-        return;
-    }
-
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'next-blog',
-        });
-
-        console.log('Connected to MongoDB');
-
-        // Check the connection state
-        const state = mongoose.connection.readyState;
-        switch (state) {
-            case 0:
-                console.log("❌ Disconnected from MongoDB");
-                break;
-            case 1:
-                console.log("✅ Successfully connected to MongoDB");
-                break;
-            case 2:
-                console.log("⏳ Connecting to MongoDB...");
-                break;
-            case 3:
-                console.log("🔄 Disconnecting from MongoDB...");
-                break;
-            default:
-                console.log("Unknown MongoDB connection state:", state);
-        }
-
-        initialized = true;
-    } catch (error) {
-        console.error('❌ Error connecting to MongoDB:', error);
-    }
+  mongoose.set('strictQuery', true);
+  if (initialized) {
+    console.log('Already connected to MongoDB');
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'next-blog',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+    initialized = true;
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
 };
