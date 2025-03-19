a0c2798543b537b973a9f612ef5e02ae18051a40
SerkanDiner <serkandiner1741@gmail.com>
2025-03-19 16:21:21 +0000

    fix the error

--- src/app/api/post/update/route.jsx
+++ src/app/api/post/update/route.jsx
@@ -1,5 +1,5 @@
-import Post from '../../../../lib/models/post.model.js';
-import { connect } from '../../../../lib/mongodb/mongoose.js';
+import Post from '../../../../lib/models/post.model.jsx';
+import { connect } from '../../../../lib/mongodb/mongoose.jsx';
 import { currentUser } from '@clerk/nextjs/server';
 
 export const PUT = async (req) => {
--- src/lib/models/post.model.jsx
+++ src/lib/models/post.model.jsx
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose.jsx';
 const postSchema = new mongoose.Schema(
   {
     userId: {
