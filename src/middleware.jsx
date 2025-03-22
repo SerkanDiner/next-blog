7a3dbaed20a873f66cdf76de68e452686650c420
SerkanDiner <serkandiner1741@gmail.com>
2025-03-22 10:04:42 +0000

    finaly fixing the errro about in route headers()

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -5,7 +5,7 @@ const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
   if (!userId && isProtectedRoute(req)) {
-    return auth().redirectToSignIn();
+    return (await auth()).redirectToSignIn();
   }
 });
 
